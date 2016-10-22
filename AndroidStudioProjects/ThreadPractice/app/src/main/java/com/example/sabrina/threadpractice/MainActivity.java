7121a54e1589bfdd8182cd73239e04490e9d3bbe
Sabrina Bell <skibri@yahoo.com>
2016-10-22 12:10:29 -0700

    Added buttons

--- AndroidStudioProjects/ThreadPractice/app/src/main/java/com/example/sabrina/threadpractice/MainActivity.java
+++ AndroidStudioProjects/ThreadPractice/app/src/main/java/com/example/sabrina/threadpractice/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sabrina.threadpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button createBtn = (Button) findViewById(R.id.create);
+        Button loadBtn = (Button) findViewById(R.id.load);
+        Button clearBtn = (Button) findViewById(R.id.clear);
+
+        createBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        loadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
