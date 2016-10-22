fff11b70ced0075cbdaca98def33b5903e9102a4
Sabrina Bell <skibri@yahoo.com>
2016-10-22 12:49:41 -0700

    Create button is set up

--- AndroidStudioProjects/ThreadPractice/app/src/main/java/com/example/sabrina/threadpractice/MainActivity.java
+++ AndroidStudioProjects/ThreadPractice/app/src/main/java/com/example/sabrina/threadpractice/MainActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,7 +24,20 @@ public class MainActivity extends AppCompatActivity {
         createBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                File file = new File(getApplicationContext().getFilesDir(), "numbers.txt");
+                BufferedWriter out = new BufferedWriter(new FileWriter(file));
+                for (int i = 1; i <= 10; i++) {
+                    try {
+                        out.write(i);
+                        out.newLine();
+                        Thread.sleep(250);
+                    }
+                    catch(IOException ex){
+                        ex.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
 
